ac4e1b050462d4b53f72e18a47ad342d9f737e09
Lucas Eduardo Hoeltgebaum <69653148+Lucas-HGB@users.noreply.github.com>
2021-07-08 09:58:20 -0300

    Generic

--- WindowsAutoInstalls/MVVM/ViewModel/HomeViewModel.cs
+++ WindowsAutoInstalls/MVVM/ViewModel/HomeViewModel.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using WindowsAutoInstalls.Core;
+using WindowsAutoInstalls.BackEnd;
 
 namespace WindowsAutoInstalls.MVVM.ViewModel
 {
-    class HomeViewModel
+    class HomeViewModel : ObservableObject
     {
+        public RelayCommand DownloadApp { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public HomeViewModel()
+        {
+            DownloadApp = new RelayCommand(o =>
+            {
+                MessageBox.Show(o.ToString());
+                // Do Stuff here
+            });
+        }
     }
 }
