fedbfe2ae488b6e9af49dbc64a9054533a0933d0
Lucas Eduardo Hoeltgebaum <69653148+Lucas-HGB@users.noreply.github.com>
2021-07-12 15:18:19 -0300

    Moved Pages of DownloadsView to ResourceDict inside DownloadsView.xaml

---
+++ WindowsAutoInstalls/BackEnd/Checks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsAutoInstalls.BackEnd
+{
+    static class Checks
+    {
+
+        public static bool FirewallStatus()
+        {
+            return true;
+        }
+    }
+}
--- WindowsAutoInstalls/MVVM/ViewModel/UtilidadesViewModel.cs
+++ WindowsAutoInstalls/MVVM/ViewModel/UtilidadesViewModel.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WindowsAutoInstalls.BackEnd;
+using WindowsAutoInstalls.Core;
 
 namespace WindowsAutoInstalls.MVVM.ViewModel
 {
-    class UtilidadesViewModel
+    class UtilidadesViewModel : ObservableObject
     {
+
+        public RelayCommand CheckFirewallStatus { get; set; }
+
+
+        public UtilidadesViewModel()
+        {
+
+
+            CheckFirewallStatus = new RelayCommand(o =>
+            {
+                Checks.FirewallStatus();
+            });
+
+        }
     }
 }
